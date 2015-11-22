0b717d71a6ffc87ccd021be02cbc129a029e2ff1
magorium <magorium@users.noreply.github.com>
2015-11-22 10:00:21 +0100

    All: add INST_DATA overload, accepting pointer for obj parameter

--- Base/Trinity/Trinity.pas
+++ Base/Trinity/Trinity.pas
@@ -4,7 +4,9 @@ unit Trinity;
 // ---------------------------------------------------------------------------
 // Edit Date   $ Entry 
 // ---------------------------------------------------------------------------
-// 2105-11-21  $ Amiga: AddAppIconA = AddAppIcon
+// 2015-11-22  $ All: Overload version of INST_DATA accepting generic pointer
+//               for object parameter
+// 2015-11-21  $ Amiga: AddAppIconA = AddAppIcon
 // 2015-11-16  $ AROS: gadtools varargs version routines
 // 2015-11-10  $ Amiga: DoDTMethod()
 //             $ MorphOS: NewDTObject(), DoDTMethod(), GetDTAttrs(), 
@@ -830,6 +832,18 @@ Const
 
   function AddAppIcon(id: ULONG; userdata: ULONG; txt: PChar; msgport: PMsgPort; lock: BPTR; diskobj: PDiskObject; const Tags: array of const): PAppIcon;
   {$ENDIF}
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+//
+//  Topic:  INST_DATA()
+//
+//////////////////////////////////////////////////////////////////////////////
+
+
+
+  function INST_DATA(cl: PIClass; o: Pointer): Pointer; overload;
 
 
 
@@ -1864,6 +1878,21 @@ begin
   AddAppIcon := AddAppIconA(id, userdata, txt, msgport, lock, diskobj, ReadInTags(Tags));
 end;
 {$ENDIF}
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+//
+//  Topic: 
+//
+//////////////////////////////////////////////////////////////////////////////
+
+
+
+function INST_DATA(cl: PIClass; o: Pointer): Pointer;
+begin
+  INST_DATA := Pointer(PtrUInt(o) + cl^.cl_InstOffset);
+end;
 
 
 
